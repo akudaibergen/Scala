dd6b6ba286fd290a7b1c4c919cdf649969a61949
akudaibergen <kudaibergen.adil@gmail.com>
2021-02-18 22:56:51 +0600

    week

---
+++ week3/src/main/scala/Main.scala
@@ -0,0 +1,13 @@
+object Hello extends App {
+  //  println("Hello, world")
+
+//  def ex1(): Unit={
+//    val x = 1
+//    var y = 2
+//    //    x = x + 1 неправильно
+//    y = y + 1
+//    println(x)
+//    println(y)
+//  }
+
+}
